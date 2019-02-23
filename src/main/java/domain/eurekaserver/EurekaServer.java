672943e206c2aaa8a632e3c6492c22600e3a3882
Joaquín <jocon@younapp.com>
2019-02-23 12:50:37 +0100

    add eureka server dependencies and main method

--- src/main/java/domain/eurekaserver/EurekaServer.java
+++ src/main/java/domain/eurekaserver/EurekaServer.java
@@ -1,4 +1,15 @@
 package domain.eurekaserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServer {
+
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServer.class, args);
+    }
 }
+
